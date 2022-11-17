23f9c9ee3464761216ee0db3a2f2a4657c429408
unknown <Sena@MEDAPRCSGFSD755.FormacionSena.RED>
2022-11-17 13:35:39 -0500

    Tercer comit

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor(){
         this.app = express();
-        this.port = process.env.port;
+        this.port = process.env.PORT;
 
         this.ventasPath = '/api/ventas';
         this.proveedoresPath = '/api/proveedores';
